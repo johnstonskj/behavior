7f0d592fb28e5713f29e37aafe6437145420f8fa
Simon Johnston <simonjo@amazon.com>
2018-08-24 15:43:03 -0700

    More coverage tests

--- behavior/test/fsm.rkt
+++ behavior/test/fsm.rkt
@@ -178,12 +178,26 @@
  (check-equal? (state-name (machine-execution-current-state started)) 'hello))
 
 (test-case
+ "execution-start: fail on second call"
+ (define exec (make-machine-execution simple-fsm))
+ (define started (execution-start exec))
+ (check-exn exn:fail:contract?
+            (λ () (execution-start started))))
+
+(test-case
  "handle-event: success"
  (define exec (make-machine-execution simple-fsm))
  (define started (execution-start exec))
  (define next (handle-event started 'wake))
  (check-equal? (machine-execution-condition next) 'completed)
  (check-equal? (state-name (machine-execution-current-state next)) 'goodbye))
+
+(test-case
+ "handle-event: fail on created"
+ (define exec (make-machine-execution simple-fsm))
+ (check-exn exn:fail:contract?
+            (λ ()
+              (handle-event exec 'wake))))
 
 (test-case
  "handle-event: fail on unknown event"
@@ -251,6 +265,14 @@
  (define next (complete-current-state started))
  (check-equal? (machine-execution-condition next) 'completed)
  (check-equal? (state-name (machine-execution-current-state next)) 'goodbye))
+
+;; TODO: check internal transition
+
+(test-case
+ "complete-current-state: fail on created"
+ (define exec (make-machine-execution blocked-fsm))
+ (check-exn exn:fail:contract?
+            (λ () (complete-current-state exec))))
 
 (test-case
  "complete-current-state: fail on no transition"
